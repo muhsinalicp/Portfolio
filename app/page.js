5c6af6d3e659aba7c13be5e8e00f4b4a190071e5
muhsinalicp <122996567+muhsinalicp@users.noreply.github.com>
2025-01-18 15:21:01 +0530

    integrated with shadcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <div>
+      <SpeedInsights />
       <Analytics />
       <h1 className="text-3xl font-bold underline">Home</h1>
     </div>
---
+++ components/fancy/letter-swap-forward-anim.jsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from "react"
+import { motion, stagger, useAnimate } from "motion/react";
+
+const LetterSwapForward = ({
+  label,
+  reverse = true,
+
+  transition = {
+    type: "spring",
+    duration: 0.7,
+  },
+
+  staggerDuration = 0.03,
+  staggerFrom = "first",
+  className,
+  onClick,
+  ...props
+}) => {
+  const [scope, animate] = useAnimate()
+  const [blocked, setBlocked] = useState(false)
+
+  const hoverStart = () => {
+    if (blocked) return
+
+    setBlocked(true)
+
+    // Function to merge user transition with stagger and delay
+    const mergeTransition = (baseTransition) => ({
+      ...baseTransition,
+      delay: stagger(staggerDuration, {
+        from: staggerFrom,
+      }),
+    })
+
+    animate(".letter", { y: reverse ? "100%" : "-100%" }, mergeTransition(transition)).then(() => {
+      animate(".letter", {
+        y: 0,
+      }, {
+        duration: 0,
+      }).then(() => {
+        setBlocked(false)
+      })
+    })
+
+    animate(".letter-secondary", {
+      top: "0%",
+    }, mergeTransition(transition)).then(() => {
+      animate(".letter-secondary", {
+        top: reverse ? "-100%" : "100%",
+      }, {
+        duration: 0,
+      })
+    })
+  }
+
+  return (
+    (<span
+      className={`flex justify-center items-center relative overflow-hidden  ${className} `}
+      onMouseEnter={hoverStart}
+      onClick={onClick}
+      ref={scope}
+      {...props}>
+      <span className="sr-only">{label}</span>
+      {label.split("").map((letter, i) => {
+        return (
+          (<span className="whitespace-pre relative flex" key={i}>
+            <motion.span className={`relative letter`} style={{ top: 0 }}>
+              {letter}
+            </motion.span>
+            <motion.span
+              className="absolute letter-secondary "
+              aria-hidden={true}
+              style={{ top: reverse ? "-100%" : "100%" }}>
+              {letter}
+            </motion.span>
+          </span>)
+        );
+      })}
+    </span>)
+  );
+}
+
+export default LetterSwapForward
